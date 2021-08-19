a8d6bb1ea796fa56aedba8ee87c2c25512db4238
Vitalii Karamanov <Vitalii_Karamanov@epam.com>
2021-08-19 14:26:04 +0300

    Laravel_project-lesson_5-connect_with_db_&_make_migrations

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        return view('admin.index', [
+            'countCategories' => 0,
+            'countNews' => count($this->newsList)
+        ]);
+    }
+}
---
+++ resources/views/admin/news/categories/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+@section('content')
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Категории</h1>
+        <a href="{{ route('admin.categories.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-plus fa-sm text-white-50"></i> Добавить новую</a>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>#ID</th>
+                        <th>Заголовок</th>
+                        <th>Текст</th>
+                        <th>Управление</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($categories as $category)
+                        <tr>
+                            <td>{{ $category->id }}</td>
+                            <td>{{ $category->title }}</td>
+                            <td>{{ $category->description }}</td>
+                            <td>
+                                <a href="{{ route('admin.categories.edit', ['category' => $category->id]) }}" style="font-size: 12px">Edit</a> &nbsp;
+                                <a href="javascript:;" style="font-size: 12px; color: red">Del.</a>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="4">Записей нет</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
